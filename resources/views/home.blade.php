4aebbe69b6c4432d6cefcee9cfd43164b537603c
Castronox <castronox@outlook.es>
2024-07-24 10:58:23 +0200

    Generada la implementación de la vista de los articulos que ha creado el propio usuario

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 
-@section('content')
+@section('contenido')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -19,5 +19,62 @@
             </div>
         </div>
     </div>
+    <br>
+    <br>
+    <br>
+    <table class="table table-striped table-bordered">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Foto</th>
+                <th>Titulo</th>
+                <th>Tema</th>
+                <th>Operaciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            {{-- {{dd($articles);}} --}}
+            @foreach ($articles as $article)
+            <tr>
+                <td>{{ $article->id }}</td>
+                <td class="text-start d-flex justify-content-center">
+                    <img class="rounded" style="max-width: 90px;"
+                        src="{{ $article->imagen ? asset('storage/' . config('filesystems.articlesImageDir')) . '/' . $article->imagen : asset('storage/' . config('filesystems.articlesImageDir')) . '/default.png' }}"
+                        alt="Imagen de {{ $article->titulo }} {{ $article->tema }}"
+                        title="Imagen de {{ $article->titulo }} {{ $article->tema }}">
+                </td>
+                <td>{{ $article->titulo }}</td>
+                <td>{{ $article->tema }}</td>
+                <td>{{ $article->visitas }}</td>
+                <td style="background-color: {{ $article->color }}">{{ $article->color }}</td>
+                <td class="text-center">
+                    <a href="{{ route('articles.show', $article->id) }}" class="btn btn-info btn-sm">
+                        <img height="20" width="20" src="{{ asset('images/buttons/show.png') }}" alt="Ver detalles"
+                            title="Ver detalles">
+                    </a>
+                    @auth
+                    @if (Auth::user()->can('update', $article))
+                    <a href="{{ route('articles.edit', $article->id) }}" class="btn btn-warning btn-sm">
+                        <img height="20" width="20" src="{{ asset('images/buttons/update.png') }}" alt="Modificar"
+                            title="Modificar">
+                    </a>
+                    @endif
+                    @can('update', $article)
+                    <a href="{{ route('articles.delete', $article->id) }}" class="btn btn-danger btn-sm">
+                        <img height="20" width="20" src="{{ asset('images/buttons/delete.png') }}" alt="Eliminar"
+                            title="Eliminar">
+                    </a>
+                    @endcan
+                    @endauth
+                </td>
+            </tr>
+            @endforeach
+            @if ($articles->isEmpty())
+            <tr>
+                <td colspan="7">No hay bicicletas registradas.</td>
+            </tr>
+            @endif
+        </tbody>
+    </table>
 </div>
 @endsection
